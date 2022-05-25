748e1399f8cfbc2b99cbb0c1189df988640c903a
Diego H <diego.hernangomezherrero@gmail.com>
2022-05-26 00:10:24 +0200

    Rebuild volcano2 tif

--- data-raw/volcano2.R
+++ data-raw/volcano2.R
@@ -3,24 +3,32 @@
 # Coords acording to https://geomorphometry.org/volcano-maungawhau/
 
 library(tidyverse)
-library(elevatr)
+# library(elevatr)
 library(terra)
+library(tidyterra)
 rast <- terra::rast("data-raw/volcano_maungawhau.asc")
 terra::crs(rast) <- pull_crs(27200)
 terra::plot(rast)
 
-tosf <- rast %>%
-  as.polygons() %>%
-  sf::st_as_sf()
+usethis::use_dev_version()
 
-newrast <- elevatr::get_elev_raster(tosf, z = 14) %>%
-  terra::rast()
 
-# Resample to same than rast
-volcano2 <- terra::resample(newrast, rast, method = "bilinear")
+# Load DEM
+# From https://data.linz.govt.nz/layer/51768-nz-8m-digital-elevation-model-2012/data/
+# tile: EK
+dem <- terra::rast("~/R/mapslib/misc/EK.tif")
+newvolcano <- terra::project(dem, rast) %>% select(elevation = EK)
+# terra::rast("inst/extdata/volcano2.tif")
+
+volcano2 <- newvolcano
+
+
+terra::plot(volcano2)
 names(volcano2) <- "elevation"
 
 unlink("inst/extdata/volcano2.tif")
 writeRaster(volcano2, "inst/extdata/volcano2.tif")
 
 aa <- terra::rast("inst/extdata/volcano2.tif")
+
+terra::plot(aa)
