b30ac6a42296761752b3ba9c3c11dbce94280929
Diego H <diego.hernangomezherrero@gmail.com>
2022-05-29 18:58:28 +0200

    Add volcano2

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Updated Topographic Information on Auckland's Maungawhau Volcano
+#'
+#' @docType data
+#'
+#' @family datasets
+#'
+#' @name volcano2
+#' @format
+#' A matrix of 109 rows and 76 columns.
+#'
+#' @source
+#'
+#' [Toitu Te Whenua Land Information New Zealand](https://data.linz.govt.nz/)
+#'
+#' 8m Digital Elevation Model (DEM) created by Geographx
+#' (<https://geographx.co.nz/>), primarily derived from January 2012 LINZ
+#' Topo50 20m contours (data.linz.govt.nz/layer/768).
+#'
+#' @seealso [volcano]
+#'
+NULL
