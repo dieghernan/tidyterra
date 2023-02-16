e3a5857ad6e24fcdca00f1cb00ec0919a11a772c
Diego Hernangómez Herrero <diego.hernangomezherrero@gmail.com>
2023-02-16 22:11:37 +0100

    Add volcano hires

---
+++ data-raw/volcano_ok.R
@@ -0,0 +1,19 @@
+library(terra)
+library(ggplot2)
+devtools::load_all()
+
+allrast <- list.files("~/R/mapslib/misc/", pattern = "^DEM_BA32.*tif", full.names = TRUE)
+dem_list <- lapply(allrast, terra::rast)
+dem <- do.call(merge, dem_list) %>% drop_na()
+
+dem_end <- spatSample(dem,size=500000, method = "regular", as.raster = TRUE)
+
+
+
+plot(dem_end)
+names(dem_end) <- "elevation"
+ncell(dem)
+ggplot() +
+  geom_spatraster(data = dem_end)
+
+writeRaster(dem_end, "./data-raw/volcano2hires.tif")
