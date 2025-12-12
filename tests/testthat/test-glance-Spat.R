961050ca5a90a0a3746bd2b3c9dc299f6c38f876
Diego H. <dieghernan@users.noreply.github.com>
2025-12-12 03:44:42 +0100

    Skip on linux

--- tests/testthat/test-glance-Spat.R
+++ tests/testthat/test-glance-Spat.R
@@ -10,6 +10,8 @@ test_that("Glance SpatRasters", {
 
 test_that("Stress SpatRaster", {
   skip_on_cran()
+  skip_on_os("linux")
+ 
   # SpatRaster
   v <- terra::rast(system.file("extdata/cyl_elev.tif", package = "tidyterra"))
   expect_snapshot(glance(v))
