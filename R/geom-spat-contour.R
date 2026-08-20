e7c4a794d071a4e21c20e6cfe0e11f9405612416
Diego H. <diego.hernangomezherrero@gmail.com>
2026-08-20 16:35:51 +0200

    Update docs

--- R/geom-spat-contour.R
+++ R/geom-spat-contour.R
@@ -2,10 +2,10 @@
 #'
 #' @description
 #'
-#' These geoms create contours of `SpatRaster` objects. To specify a valid
-#' surface, specify the layer in `aes(z = layer_name)`; otherwise, all layers
-#' are considered when creating contours. See also the **Facets**
-#' section.
+#' These geoms create contours from `SpatRaster` objects. To create contours
+#' from a specific layer, map that layer to the `z` aesthetic with
+#' `aes(z = layer_name)`. Otherwise, all layers are used to create contours.
+#' See also the **Facets** section.
 #'
 #' The underlying implementation is based on [ggplot2::geom_contour()].
 #'
